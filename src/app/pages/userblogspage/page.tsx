e6671a1ee84459de66e9a81a1f0f716af1fde1e3
khushboo <aggarwal.khushi1501@gmail.com>
2023-11-11 11:56:28 +0530

    initial commit

---
+++ src/app/pages/userblogspage/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TipTap from "../blogspage/page";
+
+function UserBlogsPage() {
+  return (
+    <>
+      <h1>Write Your Blog</h1>
+      <TipTap />
+    </>
+  );
+}
+
+export default UserBlogsPage;
