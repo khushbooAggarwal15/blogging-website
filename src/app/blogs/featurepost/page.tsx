f1826f203cd3e648d95e540ebf97228efbd63410
khushboo <aggarwal.khushi1501@gmail.com>
2023-11-14 18:37:01 +0530

    packages

--- src/app/blogs/featurepost/page.tsx
+++ src/app/blogs/featurepost/page.tsx
@@ -5,8 +5,6 @@ import Card from "@mui/material/Card";
 import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Header from "../../components/Header/header";
-import Footer from "../../components/Footer/footer";
 
 interface FeaturedPostProps {
   post: {
@@ -22,32 +20,28 @@ export default function FeaturedPost(props: FeaturedPostProps) {
   const { post } = props;
 
   return (
-    <>
-      <Header />
-      <Grid item xs={12} md={4}>
-        <CardActionArea component="a" href="#">
-          <Card sx={{ display: "flex" }}>
-            <CardContent sx={{ flex: 1 }}>
-              <Typography component="h2" variant="h5">
-                {post.title}
-              </Typography>
-              <Typography variant="subtitle1" color="text.secondary">
-                {post.date}
-              </Typography>
-              <Typography variant="subtitle1" paragraph>
-                {post.description}
-              </Typography>
-            </CardContent>
-            <CardMedia
-              component="img"
-              sx={{ width: 160, display: { xs: "none", sm: "block" } }}
-              image={post.image}
-              alt={post.imageLabel}
-            />
-          </Card>
-        </CardActionArea>
-      </Grid>
-      <Footer />
-    </>
+    <Grid item xs={12} md={4}>
+      <CardActionArea component="a" href="#">
+        <Card sx={{ display: "flex" }}>
+          <CardContent sx={{ flex: 1 }}>
+            <Typography component="h2" variant="h5">
+              {post.title}
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary">
+              {post.date}
+            </Typography>
+            <Typography variant="subtitle1" paragraph>
+              {post.description}
+            </Typography>
+          </CardContent>
+          <CardMedia
+            component="img"
+            sx={{ width: 160, display: { xs: "none", sm: "block" } }}
+            image={post.image}
+            alt={post.imageLabel}
+          />
+        </Card>
+      </CardActionArea>
+    </Grid>
   );
 }
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -4,6 +4,8 @@ import { createTheme, Container, Grid, Button } from "@mui/material";
 import React from "react";
 import FeaturedPost from "./featurepost/page";
 import "../public/images/Image.png";
+import Header from "../components/Header/header";
+import Footer from "../components/Footer/footer";
 
 const BlogsPage = () => {
   const defaultTheme = createTheme();
@@ -52,84 +54,88 @@ const BlogsPage = () => {
   ];
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container maxWidth="lg">
-        <main>
-          <div
-            className="New"
-            style={{
-              height: 484,
-              position: "relative",
-            }}
-          >
-            <img
-              className="DesignbetterWebsiteBookAssetsIllustrationsPrinciplesofproductdesignCover1920x1081"
-              style={{
-                width: 1150,
-                height: 484,
-                left: 0,
-                top: 0,
-                position: "absolute",
-              }}
-              src="https://source.unsplash.com/random?wallpapers"
-            />
+    <>
+      <Header />
+      <ThemeProvider theme={defaultTheme}>
+        <Container maxWidth="lg">
+          <main>
             <div
+              className="New"
               style={{
-                position: "absolute",
-                backgroundColor: "white",
-                marginTop: 420,
-                marginLeft: 120,
-                padding: 20,
-                zIndex: 1,
-                maxWidth: 350,
-                height: 150,
-                border: "2px solid blue",
+                height: 484,
+                position: "relative",
               }}
             >
-              <h3>
-                {" "}
-                The Impact Of Technology on The workspace : How texhnology is
-                changing
-              </h3>
+              <img
+                className="DesignbetterWebsiteBookAssetsIllustrationsPrinciplesofproductdesignCover1920x1081"
+                style={{
+                  width: 1150,
+                  height: 484,
+                  left: 0,
+                  top: 0,
+                  position: "absolute",
+                }}
+                src="https://source.unsplash.com/random?wallpapers"
+              />
+              <div
+                style={{
+                  position: "absolute",
+                  backgroundColor: "white",
+                  marginTop: 420,
+                  marginLeft: 120,
+                  padding: 20,
+                  zIndex: 1,
+                  maxWidth: 350,
+                  height: 150,
+                  border: "2px solid blue",
+                }}
+              >
+                <h3>
+                  {" "}
+                  The Impact Of Technology on The workspace : How texhnology is
+                  changing
+                </h3>
+              </div>
             </div>
-          </div>
 
-          <div
-            className="ReadingLists"
-            style={{
-              width: 236,
-              height: 65,
-              color: "black",
-              marginTop: 200,
-              fontSize: 36,
-              fontFamily: "Open Sans",
-              fontWeight: "700",
-              wordWrap: "break-word",
-            }}
-          >
-            Latest Post
-          </div>
-          <div>
-            <Grid container spacing={6}>
-              {featuredPosts.map((post) => (
-                <FeaturedPost key={post.title} post={post} />
-              ))}
-            </Grid>
-          </div>
+            <div
+              className="ReadingLists"
+              style={{
+                width: 236,
+                height: 65,
+                color: "black",
+                marginTop: 200,
+                fontSize: 36,
+                fontFamily: "Open Sans",
+                fontWeight: "700",
+                wordWrap: "break-word",
+              }}
+            >
+              Latest Post
+            </div>
+            <div>
+              <Grid container spacing={6}>
+                {featuredPosts.map((post) => (
+                  <FeaturedPost key={post.title} post={post} />
+                ))}
+              </Grid>
+            </div>
 
-          <Button
-            type="submit"
-            variant="text"
-            sx={{
-              mt: 3,
-              ml: 1,
-            }}
-          >
-            View All posts
-          </Button>
-        </main>
-      </Container>
-    </ThemeProvider>
+            <Button
+              type="submit"
+              variant="text"
+              sx={{
+                mt: 3,
+                ml: 1,
+              }}
+            >
+              View All posts
+            </Button>
+          </main>
+        </Container>
+      </ThemeProvider>
+      <Footer />
+    </>
   );
 };
 
