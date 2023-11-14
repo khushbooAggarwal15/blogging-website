c451e7acaa76947b685bb125d13dca59d99680fe
GAGAN9294 <82665827+GAGAN9294@users.noreply.github.com>
2023-11-14 18:17:08 +0530

    home page and bloglists page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import BlogsPage from "./blogs/page";
 import LoginPage from "./login/page";
 
 export default function Home() {
   return (
     <main>
-      <LoginPage />
+      <BlogsPage />
     </main>
   );
 }
