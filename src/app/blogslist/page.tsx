b1d192e87bc8b416de120f87495bca6668775145
khushboo <aggarwal.khushi1501@gmail.com>
2023-11-14 18:34:21 +0530

    packages

--- src/app/blogslist/page.tsx
+++ src/app/blogslist/page.tsx
@@ -4,6 +4,8 @@ import { createTheme, Container, Grid, Button } from "@mui/material";
 import React from "react";
 import FeaturedPost from "../blogs/featurepost/page";
 import "../public/images/Image.png";
+import Header from "../components/Header/header";
+import Footer from "../components/Footer/footer";
 
 const BlogsPage = () => {
   const defaultTheme = createTheme();
@@ -52,81 +54,85 @@ const BlogsPage = () => {
   ];
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container maxWidth="lg">
-        <main>
-          <div
-            className="New"
-            style={{
-              top: 80,
-              height: 484,
-              position: "relative",
-            }}
-          >
-            <img
-              className="DesignbetterWebsiteBookAssetsIllustrationsPrinciplesofproductdesignCover1920x1081"
-              style={{
-                width: 1150,
-                height: 484,
-                left: 0,
-                top: 0,
-                zIndex: -1,
-                position: "absolute",
-              }}
-              src="https://source.unsplash.com/random?wallpapers"
-            />
+    <>
+      <Header />
+      <ThemeProvider theme={defaultTheme}>
+        <Container maxWidth="lg">
+          <main>
             <div
+              className="New"
               style={{
-                position: "absolute",
-                color: "white",
-                marginTop: 400,
-                marginLeft: 80,
+                top: 80,
+                height: 484,
+                position: "relative",
               }}
             >
-              <h2>
-                {" "}
-                The Impact Of Technology on The workspace : How texhnology is
-                changing
-              </h2>
+              <img
+                className="DesignbetterWebsiteBookAssetsIllustrationsPrinciplesofproductdesignCover1920x1081"
+                style={{
+                  width: 1150,
+                  height: 484,
+                  left: 0,
+                  top: 0,
+                  zIndex: -1,
+                  position: "absolute",
+                }}
+                src="https://source.unsplash.com/random?wallpapers"
+              />
+              <div
+                style={{
+                  position: "absolute",
+                  color: "white",
+                  marginTop: 400,
+                  marginLeft: 80,
+                }}
+              >
+                <h2>
+                  {" "}
+                  The Impact Of Technology on The workspace : How texhnology is
+                  changing
+                </h2>
+              </div>
             </div>
-          </div>
 
-          <div
-            className="ReadingLists"
-            style={{
-              width: 236,
-              height: 65,
-              color: "black",
-              marginTop: 200,
-              fontSize: 36,
-              fontFamily: "Open Sans",
-              fontWeight: "700",
-              wordWrap: "break-word",
-            }}
-          >
-            Blogs List
-          </div>
-          <div>
-            <Grid container spacing={6}>
-              {featuredPosts.map((post) => (
-                <FeaturedPost key={post.title} post={post} />
-              ))}
-            </Grid>
-          </div>
+            <div
+              className="ReadingLists"
+              style={{
+                width: 236,
+                height: 65,
+                color: "black",
+                marginTop: 200,
+                fontSize: 36,
+                fontFamily: "Open Sans",
+                fontWeight: "700",
+                wordWrap: "break-word",
+              }}
+            >
+              Blogs List
+            </div>
+            <div>
+              <Grid container spacing={6}>
+                {featuredPosts.map((post) => (
+                  <FeaturedPost key={post.title} post={post} />
+                ))}
+              </Grid>
+            </div>
 
-          <Button
-            type="submit"
-            variant="contained"
-            sx={{
-              mt: 3,
-              ml: 1,
-            }}
-          >
-            Load More
-          </Button>
-        </main>
-      </Container>
-    </ThemeProvider>
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{
+                mt: 3,
+                ml: 1,
+              }}
+            >
+              Load More
+            </Button>
+          </main>
+        </Container>
+      </ThemeProvider>
+      <Footer />
+    </>
   );
 };
 
